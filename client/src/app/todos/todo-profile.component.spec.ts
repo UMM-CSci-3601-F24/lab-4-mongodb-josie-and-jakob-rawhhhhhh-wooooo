1c91b5e39c977c29f5a002f88e6f882fb3de93f2
JosieBarber <barbe497@umn.edu>
2024-09-30 20:50:10 -0500

    Add some client side tests

--- client/src/app/todos/todo-profile.component.spec.ts
+++ client/src/app/todos/todo-profile.component.spec.ts
@@ -1,23 +1,54 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { throwError } from 'rxjs';
+import { ActivatedRouteStub } from '../../testing/activated-route-stub';
+import { MockTodoService } from '../../testing/todo.service.mock';
+import { Todo } from './todo';
+import { TodoCardComponent } from './todo-card.component';
 import { TodoProfileComponent } from './todo-profile.component';
+import { TodoService } from './todo.service';
 
 describe('TodoProfileComponent', () => {
   let component: TodoProfileComponent;
   let fixture: ComponentFixture<TodoProfileComponent>;
+  const mockTodoService = new MockTodoService();
+  const chrisId = 'chris_id';
+  const activatedRoute: ActivatedRouteStub = new ActivatedRouteStub({
+    // Using the constructor here lets us try that branch in `activated-route-stub.ts`
+    // and then we can choose a new parameter map in the tests if we choose
+    id : chrisId
+  });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [TodoProfileComponent]
-    })
-    .compileComponents();
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+    imports: [
+        RouterTestingModule,
+        MatCardModule,
+        TodoProfileComponent,
+        TodoCardComponent
+    ],
+    providers: [
+        { provide: TodoService, useValue: mockTodoService },
+        { provide: ActivatedRoute, useValue: activatedRoute }
+    ]
+  }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(TodoProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to a specific todo profile', () => {
+    const expectedTodo: Todo = MockTodoService.testTodos[0];
+    activatedRoute.setParamMap({ id: expectedTodo._id });
+    expect(component.todo()).toEqual(expectedTodo);
   });
 });
--- client/src/testing/todo.service.mock.ts
+++ client/src/testing/todo.service.mock.ts
@@ -7,21 +7,21 @@ import { TodoService } from '../app/todos/todo.service';
 export class MockTodoService extends TodoService {
   static testTodos: Todo[] = [
     {
-      _id: '58895985a22c04e761776d54',
+      _id: '1',
       owner: 'Blanche',
       status: false,
       body: 'In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.',
       category: 'software design',
     },
     {
-      _id: '58895985c1849992336c219b',
+      _id: '22',
       owner: 'Fry',
       status: false,
       body: 'Ipsum esse est ullamco magna tempor anim laborum non officia deserunt veniam commodo. Aute minim incididunt ex commodo.',
       category: 'video games',
     },
     {
-      _id: '58895985ae3b752b124e7663',
+      _id: '3',
       owner: 'Fry',
       status: true,
       body: 'Ullamco irure laborum magna dolor non. Anim occaecat adipisicing cillum eu magna in.',
